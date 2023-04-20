e393601915602276211fbdf10ccfd4a1a304603f
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-20 23:31:13 +0300

    ES6_basic

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,4 @@
+export default function iterateThroughObject(reportWithIterator) {
+  // Join the employee names in the reportWithIterator array with '|' separator
+  return reportWithIterator.join(' | ');
+}
