32fce2496ace1b02712647ed88d6f8fba9295994
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-25 14:21:24 +0300

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outsiderange');
   }
   const buffer = new ArrayBuffer(length);
-  const int8Array = new DataView(buffer);
+  const int8Array = new DataView(buffer, 0);
   int8Array.setInt8(position, value);
   return int8Array;
 }
