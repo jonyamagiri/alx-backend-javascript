8fb284e69c1b6ba704718a566b99e6dba1d298b1
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 01:02:57 +0300

    TypeScript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+// build a Teacher interface as per specifications
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    location: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    [propName: string]: any;
+}
