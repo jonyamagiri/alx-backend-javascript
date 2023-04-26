2d6b247e96adba5dfd26304a014dc48d1076e27b
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 01:36:49 +0300

    TypeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,24 +1,52 @@
 // build a Teacher interface as per specifications
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    location: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    [propName: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  [propName: string]: any;
 }
 
 // interface named Directors that extends Teacher
 interface Directors extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 }
 
 // a function printTeacher (usage: printTeacher("John", "Doe") -> J. Doe)
 export interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-  }
-  
-  export const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    return `${firstName.charAt(0)}. ${lastName}`;
-  };
+  (firstName: string, lastName: string): string;
+}
 
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+//   a Class named StudentClass
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
