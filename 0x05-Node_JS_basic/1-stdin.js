7444100205220bd02f8fdda9476a647e100c49e2
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-13 13:11:13 +0300

    Node_JS_basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) {
+  if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
